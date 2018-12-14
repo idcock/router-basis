fb17faff79b92b105bc2f028294af65a3f810085
ira <iraschooltools@gmail.com>
2018-12-14 15:50:00 +0100

    init

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 
@@ -13,13 +13,6 @@ export default new Router({
       name: 'home',
       component: Home
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    
   ]
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main class="home">
+    <h1> Welkom bij Beau-tique </h1>
+    <h2> Ontdek onze producten en leer meer over ons</h2>
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+   
   }
 }
 </script>
+
+<style scoped>
+.home {
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  padding: 5rem 3rem;
+}
+
+h1 {
+  font-size: 7rem;
+  color:#40B782;
+
+}
+
+h2 {
+  font-size: 3rem;
+  color:black;
+}
+</style>
