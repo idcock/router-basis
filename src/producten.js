d28c4e8d7c6292f0978314f75afa40d6d3c6d216
ira <iraschooltools@gmail.com>
2019-01-22 20:11:15 +0100

    versie 3

---
+++ src/producten.js
@@ -0,0 +1,78 @@
+const producten = [
+    {
+        id: 1,
+        naam: "Kocca",
+        beschrijving: "Ecru Tshirt met Multicolor Afboordingen Kocca",
+        categorie: "t-shirts",
+        prijs: 62.90,
+        afbeelding: "kocca.jpg",
+        geslacht: "vrouw"
+    },
+    {
+        id: 2,
+        naam: "Patrizia Pepe",
+        beschrijving: "Witte Sjaal met Zilvere Metallicdraaden Logo Patrizia Pepe",
+        categorie: "accessoires",
+        prijs: 128,
+        afbeelding: "patrizia.jpg",
+        geslacht: "vrouw"
+    },
+    {
+        id: 3,
+        naam: "Tommy Hilfiger",
+        beschrijving: "Donkerblauwe T-shirt met Witte en Fluogele Opdruk Tommy Hilfiger",
+        categorie: "t-shirts",
+        prijs: 24.95,
+        afbeelding: "tommy.jpg",
+        geslacht: "man"
+    },
+    {
+        id: 4,
+        naam: "Armani Junior",
+        beschrijving: "Grijze Short Armani Communie Jongens",
+        categorie: "broeken",
+        prijs: 87.95,
+        afbeelding: "armani.jpg",
+        geslacht: "man"
+    },
+    {
+        id: 5,
+        naam: "Guess",
+        beschrijving: "Blauwe JEANS GIRLS CASUEL TEENS SKINNY FIT Guess Girls",
+        categorie: "broeken",
+        prijs: 49.95,
+        afbeelding: "guess.jpg",
+        geslacht: "vrouw"
+    },
+    {
+        id: 6,
+        naam: "Noali",
+        beschrijving: "Blauwe JEANS GIRLS CASUEL TEENS SKINNY FIT Guess Girls",
+        categorie: "tops",
+        prijs: 203.95,
+        afbeelding: "noali.jpg",
+        geslacht: "vrouw"
+    },
+    {
+        id: 7,
+        naam: "Noali",
+        beschrijving: "Blauw Jeansjasje afgebiesd met gouden boord Noali",
+        categorie: "jassen",
+        prijs: 139.95,
+        afbeelding: "noali.jpg",
+        geslacht: "vrouw"
+    },
+];
+
+const categories = [
+    "t-shirts",
+    "accessoires",
+    "broeken",
+    "tops",
+    "jassen",
+];
+
+export {
+    categories,
+    producten
+}
--- src/views/Producten.vue
+++ src/views/Producten.vue
@@ -1,19 +1,45 @@
 <template>
-    <div class="contact">
+    <div class="producten">
         <h1>Producten</h1>
         <h2>Bekijk onze fantastische producten</h2>
+        <div class="producten-lijst">
+            <div class="product-kaart" v-for="product of producten" :key="product.id">
+                 <header class="product-header">    
+                    {{product.naam}}       
+                </header>
+                <figure class="image-container">
+                    <img :src="maakAfbeeldingUrl(product)"
+                        :alt = "`afbeelding van ${product.naam}`" >
+                </figure>
+                <footer class = "product-prijs">
+                    &euro;{{product.prijs}}
+                </footer>
+            </div>
+        </div>
     </div>
     
 </template>
 
 <script>
-export default {
+// @ is een alias voor de rootmap (src)
+import {producten,categories} from '@/producten.js'
 
+export default {
+    data() {
+        return {
+            //we stoppen in variabele producten de geimporteerde array producten
+            // met enkel te importeren kunnen we niet aan de producten
+            producten:producten,
+            categories
+
+        }
+    },
+   
 }
 </script>
 
 <style scoped>
-.contact {
+.producten {
     display:flex;
     flex-direction: column;
     align-items:center;
